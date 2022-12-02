b82e45928901b51a25d0e3b7e5311bb1f8a8efc0
benji <Benji59btw@gmail.com>
2022-12-02 13:56:54 +0100

    exo 82 benjamin

---
+++ exo.js
@@ -0,0 +1,65 @@
+//nombre à arrondir inférieur
+let inferieur = 34.589456;
+let inferieur1 = 68.412784;
+
+inferieur = Math.floor(inferieur)
+console.log(inferieur)
+inferieur1 = Math.floor(inferieur1)
+console.log(inferieur1)
+
+//nombre à arrondir supérieur
+let superieur = 39.456
+let superieur1 = 27.34876541
+
+superieur = Math.ceil(superieur)
+console.log(superieur)
+superieur1 = Math.ceil(superieur1)
+console.log(superieur1)
+
+//arrondir round
+let round = 5.68
+let round1 = 5.41
+
+round = Math.round(round)
+console.log(round)
+round1 = Math.round(round1)
+console.log(round1)
+
+//suppr la partie décimale
+let suppr = 6.12457
+let suppr1 = 9.568124
+
+suppr = Math.trunc(suppr)
+console.log(suppr)
+suppr1 = Math.trunc(suppr1)
+console.log(suppr1)
+
+//valeur min
+let a = 5, b = 1, c = 12, d = -1, e = 45;
+let minimum = Math.min(a, b, c, d, e);
+console.log(minimum)
+
+let f = 9, g = 12, h = 16, i = 44, j = 3;
+let minimum1 = Math.min(f, g, h, i, j);
+console.log(minimum1)
+
+//valeur max
+let a1 = 5, b1 = 1,c1 = 12, d1 = -1, e1 = 45;
+let maximum = Math.max(a1, b1, c1, d1, e1)
+console.log(maximum)
+
+let f1 = 9, g1 = 12, h1 = 16, i1 = 44, j1 = 3;
+let maximum1 = Math.max(f1, g1 ,h1, i1, j1)
+console.log(maximum1)
+
+//absolute
+let absolute = Math.abs(-6);
+console.log(absolute);
+
+let absolute1 = Math.abs(-56)
+console.log(absolute1)
+
+
+
+
+
